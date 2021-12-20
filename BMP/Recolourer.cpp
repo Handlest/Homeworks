1fd69609b9b30643670e20d14722b16978b753b0
Handlest <91721625+Handlest@users.noreply.github.com>
2021-12-20 10:57:04 +0300

    Update Recolourer.cpp

--- BMP/Recolourer.cpp
+++ BMP/Recolourer.cpp
@@ -2,18 +2,16 @@
 #include "ImgFunctions.h"
 #include <iostream>
 int main() {
-        RgbImg img = readRgbImg("totaly_normal_rainbow.bmp");
-        RgbImg recoloured = createRgbImg(img.height, img.width);
-        for (size_t row = 0; row < img.width; ++row)
-            for (size_t col = 0; col < img.height; ++col)
-            {
-                int tmp;
-                tmp = img.pixels[col][row].Red;
-                recoloured.pixels[col][row].Red = img.pixels[col][row].Blue;
-                recoloured.pixels[col][row].Blue = tmp;
-            }
-        writeRgbImg("recoloured_rainbow.bmp", recoloured);
-        deleteRgbImg(img);
-        deleteRgbImg(recoloured);
-        return 0;
-    }
+    RgbImg img = readRgbImg("totaly_normal_rainbow.bmp");
+    RgbImg recoloured = createRgbImg(img.height, img.width);
+    for (size_t row = 0; row < img.width; ++row)
+        for (size_t col = 0; col < img.height; ++col){
+            recoloured.pixels[col][row].Red = img.pixels[col][row].Blue;
+            recoloured.pixels[col][row].Blue = img.pixels[col][row].Red;
+            recoloured.pixels[col][row].Green = img.pixels[col][row].Green;
+        }
+    writeRgbImg("recoloured_rainbow.bmp", recoloured);
+    deleteRgbImg(img);
+    deleteRgbImg(recoloured);
+    return 0;
+}
