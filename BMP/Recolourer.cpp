358656349718b705a745659da0703cec6ea8af7c
Handlest <91721625+Handlest@users.noreply.github.com>
2021-12-20 11:02:46 +0300

    Update Recolourer.cpp

--- BMP/Recolourer.cpp
+++ BMP/Recolourer.cpp
@@ -5,7 +5,7 @@ int main() {
     RgbImg img = readRgbImg("totaly_normal_rainbow.bmp");
     for (size_t row = 0; row < img.width; ++row)
         for (size_t col = 0; col < img.height; ++col){
-            int tmp = img.pixels[col][row].Blue;
+            BYTE tmp = img.pixels[col][row].Blue;
             img.pixels[col][row].Blue = img.pixels[col][row].Red;
             img.pixels[col][row].Red = tmp;
         }
